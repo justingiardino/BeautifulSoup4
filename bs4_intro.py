c8e4a50abd7fb1e65054753d4dd2942b3e7ede7b
justingiardino13 <justingiardino13@gmail.com>
2019-05-08 16:28:43 -0700

    BeginningIntro

---
+++ bs4_intro.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+from get_intro import simple_get
+
+def demo_select():
+    raw_html = open('example.html').read()
+    #pass the raw html into the Beautiful Soup constructor, if you omit the html.parser then you will get a warning
+    html = BeautifulSoup(raw_html, 'html.parser')
+    #use select to locate the elements in the document, in this case looking for p for paragraph elements
+    for p in html.select('p'):
+        if p['id'] == 'walrus':
+            print(p.text)
+
+def math_names():
+    #Using mathemetician site to get names
+    math_raw_html = simple_get('http://www.fabpedigree.com/james/mathmen.htm')
+    math_html = BeautifulSoup(math_raw_html, 'html.parser')
+    #to determine what tag you want to select, use the F12 dev tools in your browser and find the tag that you want, usually an id or class element
+    #in this lase we are using li as our select
+    #use a set to get rid of duplicates
+    names = set()
+    #using enumerate function, which will loop through the list of math_html.select values and store each element as li, and also keep a counter in i
+    #for  i, li in enumerate(math_html.select('li')):
+    for li in math_html.select('li'):
+        #print(i, li.text)
+        for name in li.text.split('\n'):
+            if len(name) > 0:
+                names.add(name.strip())
+    print(list(names))
+
+
+if __name__ == '__main__':
+    demo_select()
+    math_names()
---
+++ get_intro.py
@@ -0,0 +1,25 @@
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+
+#using HTTP GET to return text contents of a website
+def simple_get(url):
+    try:
+        #function closing will free up any network resources once they leave this block of code
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+    except RequestException as e:
+        print("Error during request to {} : {}".format(url, str(e)))
+
+def is_good_response(resp):
+    content_type = resp.headers['Content-Type'].lower()
+    return(resp.status_code == 200 and content_type is not None and content_type.find('html') > -1)
+
+if __name__ == '__main__':
+    raw_html = simple_get('https://realpython.com/python-web-scraping-practical-introduction/')
+    print(len(raw_html))
+    #print(raw_html)
